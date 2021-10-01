e5248ba48e3afea2c257d83f0f7c64081ff29899
Park Seo Joon <87398748+ParkSeoJoon2005@users.noreply.github.com>
2021-10-01 17:17:15 +0530

    Update musicplay.py

--- AnkiVector/modules/musicplay.py
+++ AnkiVector/modules/musicplay.py
@@ -1,9 +1,2 @@
-__help__ = """
-My channel Is Andromeda Telescopes,That Channel Don't Want  amusic Player
-And This Bot For That Channel
 
 
-So Why I play Music?
-Music Player Is UnAvailable 
-"""
-__mod_name__ = "Music Play"
